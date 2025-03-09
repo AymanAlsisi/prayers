547b60781b55480cef4f0f8bb36faa77ca4c5888
AymanAlsisi <151306853+AymanAlsisi@users.noreply.github.com>
2025-03-10 01:06:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -33,24 +33,19 @@ const Cities = [
 ]
 
 document.querySelector('select').addEventListener("change", () => {
-    document.querySelector('.prayers').innerHTML = `<h1 class="loading"></h1>`
+    document.querySelector('.prayers').innerHTML = `<p class="loading"></p>`
 })
 
-setInterval(() => {document.querySelector('.loading').innerHTML += "."}, 400)
-setInterval(() => {document.querySelector('.loading').innerHTML = ""}, 1400)
+setInterval(() => { document.querySelector('.loading').innerHTML += "." }, 400)
+setInterval(() => { document.querySelector('.loading').innerHTML = "" }, 1400)
 
 
 const getPrayerTimings = (city) => {
-    if (city === "") {
-        document.querySelector('.prayers').innerHTML = 
-        `<div class="empty">
-            <h1 class="empty-text">لم يتم تحديد مدينة بعد</h1>
-        </div>`
-    }
-    else if (city === "no") {
-        document.querySelector('.prayers').innerHTML = 
-        `<div class="empty">
-            <h1 class="empty-text">لم يتم تحديد مدينة بعد</h1>
+    let state;
+    if (city === "" || city === "no") {
+        document.querySelector('.prayers').innerHTML =
+            `<div class="empty">
+            <p class="empty-text">لم يتم تحديد مدينة بعد</p>
         </div>`
     } else {
         axios.get(`https://api.aladhan.com/v1/timingsByAddress/${myDate}?address=${city}%20Sudan`)
@@ -60,72 +55,92 @@ const getPrayerTimings = (city) => {
                 document.querySelector('.prayers').innerHTML =
                     `<h3 id="head-hiri">${hiri}</h3>
                     <ul class="prayers-list">
-                        <li><p class="prayers-title">العشاء</p><p>${prayers.Isha}</p></li>
-                        <li><p class="prayers-title">المغرب</p><p>${prayers.Maghrib}</p></li>
-                        <li><p class="prayers-title">العصر</p><p>${prayers.Asr}</p></li>
-                        <li><p class="prayers-title">الظهر</p><p>${prayers.Dhuhr}</p></li>
-                        <li><p class="prayers-title">الشروق</p><p>${prayers.Sunrise}</p></li>
-                        <li><p class="prayers-title">الفجر</p><p>${prayers.Fajr}</p></li>
+                    <li><p class="prayer-title">الفجر</p><p>${prayers.Fajr}</p></li>
+                    <li><p class="prayer-title">الشروق</p><p>${prayers.Sunrise}</p></li>
+                    <li><p class="prayer-title">الظهر</p><p>${prayers.Dhuhr}</p></li>
+                    <li><p class="prayer-title">العصر</p><p>${prayers.Asr}</p></li>
+                    <li><p class="prayer-title">المغرب</p><p>${prayers.Maghrib}</p></li>
+                    <li><p class="prayer-title">العشاء</p><p>${prayers.Isha}</p></li>
                     </ul>`
             })
         if (city === 'Atabara') {
             city = 'عطبرة'
+            state = "ولاية نهر النيل"
         }
         else if (city === 'Khartoum') {
             city = 'الخرطوم'
+            state = "ولاية الخرطوم"
         }
         else if (city === 'Port Sudan') {
             city = 'بورتسودان'
+            state = "ولاية البحر الأحمر"
         }
         else if (city === 'Wd Madani') {
             city = 'ود مدني'
+            state = "ولاية الجزيرة"
         }
         else if (city === 'Wadi Halfa') {
             city = 'وادي حلفا'
+            state = "الولاية الشمالية"
         }
         else if (city === 'Al Qadarif') {
             city = 'القضارف'
+            state = "ولاية القضارف"
         }
         else if (city === 'Nyala') {
             city = 'نيالا'
+            state = "ولاية جنوب دارفور"
         }
         else if (city === 'Kasala') {
             city = 'كسلا'
+            state = "ولاية كسلا"
         }
         else if (city === 'Al Fashir') {
             city = 'الفاشر'
+            state = "ولاية شمال دارفور"
         }
         else if (city === 'Al Ubayyid') {
             city = 'الأبيّض'
+            state = "ولاية شمال كردفان"
         }
         else if (city === 'Kusti') {
             city = 'كوستي'
+            state = "ولاية النيل الأبيض"
         }
         else if (city === 'Dongola') {
             city = 'دنقلا'
+            state = "الولاية الشمالية"
         }
         else if (city === 'Kadugli') {
             city = 'كادقلي'
+            state = "ولاية جنوب كردفان"
         }
         else if (city === 'Al Damazin') {
             city = 'الدمازين'
+            state = "ولاية النيل الأزرق"
         }
         else if (city === 'Zalingei') {
             city = 'زالنجي'
+            state = "ولاية وسط دارفور"
         }
         else if (city === 'Al Deain') {
             city = 'الضعين'
+            state = "ولاية شرق دارفور"
         }
         else if (city === 'Al Jinena') {
             city = 'الجنينة'
+            state = "ولاية جنوب دارفور"
         }
         else if (city === 'Sennar') {
             city = 'سنار'
+            state = "ولاية سنار"
         }
         else if (city === 'Al Fulah') {
             city = 'الفولة'
+            state = "ولاية غرب كردفان"
         }
         document.querySelector("#head-city").innerHTML = city
+        document.querySelector("#head-state").innerHTML = state
     }
 }
 
